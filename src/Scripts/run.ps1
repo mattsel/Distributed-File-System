304b8cb6573c399869e9cc0168e7d42cf63e3865
mattsel <matt.sel04@gmail.com>
2024-11-24 19:58:50 -0500

    Added run scripts for testing

---
+++ src/Scripts/run.ps1
@@ -0,0 +1,14 @@
+cd ..\DistributedFileSystem.WorkerNode
+dotnet clean
+dotnet build
+dotnet run
+
+cd ..\DistributedFileSystem.MasterNode
+dotnet clean
+dotnet build
+dotnet run
+
+cd ..\DistributedFileSystem.Client
+dotnet clean
+dotnet build
+dotnet run
